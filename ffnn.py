39248f24e16b3555f703195322a94a4416160630
NP <ninasopopp@gmail.com>
2024-10-31 01:07:54 +0100

    binary touch up but missing mc

--- ffnn.py
+++ ffnn.py
@@ -7,13 +7,13 @@ Implementation of simple feed forward neural network
 import numpy as np
 
 #weights
-w1 = np.matrix([[0.5, 0.1], [-0.8, 0.1],[0.9, -0.1], [0.4, -0.4]])
-w2 = np.matrix([[0.8, 0.2],[-0.7, 0.3]])
-w3 = np.matrix([[0.9],[0.4]])
+w1 = np.array([[0.5, 0.1], [-0.8, 0.1],[0.9, -0.1], [0.4, -0.4]])
+w2 = np.array([[0.8, 0.2],[-0.7, 0.3]])
+w3 = np.array([[0.9],[-0.4]])
 #biases
-b1 = np.matrix([[0.05], [0.01]])
-b2 = np.matrix([[-0.05],[0.3]])
-b3 = np.matrix([[-0.01]])
+b1 = np.array([[0.05], [0.01]])
+b2 = np.array([[-0.05],[0.3]])
+b3 = np.array([[-0.01]])
 
 def ReLU(x):
     return np.maximum(0,x)
@@ -23,12 +23,10 @@ def sigmoid(x):
 
 #oof unsure
 def softmax(x):
-    eho = np.exp(x)
-    return eho/sum(eho)
+    return np.exp(x)/sum(np.exp(x))
 
 def FFNN(input_vector, mode="binary"):
     x = input_vector
-#this is where the maths should be mathing but instead... uh i need to come back to this
     
     hl1 = ReLU(np.dot(x, w1)+b1)
     x = hl1
@@ -36,9 +34,14 @@ def FFNN(input_vector, mode="binary"):
     out = np.dot(hl2, w3)+b3
 
     if mode == "binary":
+        #was supposed to return probability as one value
+        out = np.dot(hl2, w3) + b3
         return sigmoid(out)
     elif mode == "multiclass":
+        #returns probability as vector
+        out1 = np.dot(hl1, w3) + b2
         return softmax(out)
-#hm
-input = np.matrix([0,2,1,4])
+
+input = np.array([0,2,1,4])
 FFNN(input, mode = "binary")
+#def need to come back and redo
